9c458762fc623416be88024475d753d4fb0b6775
Udaldi <48814336+Udaldi@users.noreply.github.com>
2019-03-22 20:17:32 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -2,9 +2,17 @@
 var current= 1;
 var total= 4;
 var max_width_xs= 767;
+var fadingMs= 100;
+// var well=document.getElementById('well');
+var well=$("#well");
+var dot1=$("#dot1");
+var dot2=$("#dot2");
+var dot3=$("#dot3");
 
 $(document).ready(function() {
 	var pagetop = $('.pagetop');
+	
+	thinking();
 	
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 100) {
@@ -31,8 +39,28 @@ $(document).ready(function() {
 			return false;
 		});
 	});
+	
+	
 })
 
+function thinking() {
+
+	well.fadeIn(2000, function() { 
+		dot1.fadeIn(1000, function() {
+			dot2.fadeIn(1000, function() {
+				dot3.fadeIn(1000, desaparecer);
+			});
+		});
+	});
+}
+
+function desaparecer() {
+
+	well.fadeOut(2000);
+	dot1.fadeOut(2000);
+	dot2.fadeOut(2000);
+	dot3.fadeOut(2000, thinking);
+}
 
 function handleClickLink(event) {
 	const block = event.currentTarget.dataset.block;
@@ -43,9 +71,7 @@ document.querySelectorAll('.link').forEach((elem) => {
 	elem.addEventListener('click', handleClickLink);
 });
 
-
 document.getElementById('go_prev').addEventListener('click', go_prev);
-
 document.getElementById('go_next').addEventListener('click', go_next);
 
 function go_next(event) {
