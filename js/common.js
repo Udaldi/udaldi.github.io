e3b9b9fc00803cc62c7638d5fe9cd99bed4e1294
Udaldi <48814336+Udaldi@users.noreply.github.com>
2019-03-30 00:09:48 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -14,7 +14,7 @@ var pagetop = $('.pagetop');
 var workBG =$("#work_bg");
 var w_index=0;
 var w_total=3;
-var workList = ['./img/worksBG.jpg', './img/workBG03.png', './img/workBG04.png'];
+var workList = ['./img/worksBG.jpg', './img/workBG03.png', './img/workBG05.png'];
 
 $(document).ready(function() {
 		
