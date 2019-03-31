7db4ec726fd9c05d9996c16e37a166db71eefafe
Udaldi <48814336+Udaldi@users.noreply.github.com>
2019-03-31 14:16:06 +0900

    Add files via upload

--- js/works.js
+++ js/works.js
@@ -2,11 +2,14 @@
 
 var max_width_xs=767;
 var pagetop = $('.pagetop');
-var workBG =$("#graphicNovel_bg");
 var w_index=0;
-var w_total=4;
-var workList = ['./img/workBG03.png', './img/workBG05.png', './img/workBG03.png', './img/workBG05.png'];
-var captureList = ["2003 -No title-","2007 -Corvus 2013-","2009 -Song for Uryes-","2007 -Corvus 2013-"];
+var w_total=2;
+var graphList = ['./img/song4uryes.png', './img/corvus2013.jpg'];
+var graphCapList = ["Song for Uryes -2019-", "Corvus2013 -2009-"];
+var p_index=0;
+var p_total=2;
+var picList = ['./img/picBG03.jpg', './img/worksBG.jpg'];
+var picCapList = ["-2019-", "-2019-", "-2019-", "-2019-"];
 
 $(document).ready(function() {
 		
@@ -37,10 +40,10 @@ $(document).ready(function() {
 	});
 	
 	alternateWorks();
+	alternatePics();
 })
 
 function alternateWorks1() {
-
 	$('#captureText').toggleClass('type');
 	
 	setTimeout(function(){ 
@@ -48,18 +51,41 @@ function alternateWorks1() {
 	}, 2800);
 }
 function alternateWorks() {
-	//document.getElementById('captureText').classList.remove("type");
 	w_index = w_index + 1;
 	
 	if(w_index >= w_total) {
 		w_index = 0;
 	}
 	
-	document.getElementById('graphicNovel_bg').style.backgroundImage = "url(" + workList[w_index] +  ")";
-	document.getElementById('captureText').innerHTML = captureList[w_index];
+	document.getElementById('graphicNovel_bg').style.backgroundImage = "url(" + graphList[w_index] +  ")";
+	document.getElementById('captureText').innerHTML = graphCapList[w_index];
 	$('#captureText').toggleClass('type');
 	
 	setTimeout(function(){ 
 		alternateWorks1();
+	}, 10000);
+
+}
+
+function alternatePics1() {
+	$('#picCapText').toggleClass('type');
+	
+	setTimeout(function(){ 
+		alternatePics();
+	}, 2800);
+}
+function alternatePics() {
+	p_index = p_index + 1;
+	
+	if(p_index >= p_total) {
+		p_index = 0;
+	}
+	
+	document.getElementById('photo_bg').style.backgroundImage = "url(" + picList[p_index] +  ")";
+	document.getElementById('picCapText').innerHTML = picCapList[p_index];
+	$('#picCapText').toggleClass('type');
+	
+	setTimeout(function(){ 
+		alternatePics1();
 	}, 10000);
 }
